9b9133da3f2ec5d346044a610e782f076f7cd95a
ritikdolly <devwithritik200@gmail.com>
2026-02-22 11:59:04 +0530

    problem 868. Binary Gap

--- february2026/BinaryGap.java
+++ february2026/BinaryGap.java
@@ -3,7 +3,8 @@
 // https://leetcode.com/problems/binary-gap/?envType=daily-question&envId=2026-02-22
 
 public class BinaryGap {
-    // method 1: using bit manipulation to find the positions of 1s and calculate the maximum distance
+    // method 1: using bit manipulation to find the positions of 1s and calculate
+    // the maximum distance
     class Solution {
 
         public int binaryGap(int n) {
@@ -27,11 +28,29 @@ public class BinaryGap {
             return maxDistance;
         }
     }
+
+    // method 2: using bit manipulation to find the positions of 1s and calculate
+    // the maximum distance (alternative approach)
+    // class Solution {
+    //     public int binaryGap(int N) {
+    //         int indexOne = -1, longestDistance = 0;
+    //         for (int i = 0; i < 32; i++) {
+    //             if (((N >> i) & 1) == 1) {
+    //                 if (indexOne != -1)
+    //                     longestDistance = Math.max(longestDistance, i - indexOne);
+    //                 indexOne = i;
+    //             }
+    //         }
+
+    //         return longestDistance;
+    //     }
+    // }
+
     public static void main(String[] args) {
         Solution solution = new BinaryGap().new Solution();
         int n = 22; // binary representation is 10110
         int result = solution.binaryGap(n);
         System.out.println("Maximum distance between two consecutive 1s: " + result);
     }
-    
+
 }
