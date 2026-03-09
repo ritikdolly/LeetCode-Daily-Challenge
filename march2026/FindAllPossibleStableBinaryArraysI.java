41b9b70afb3468c9bc63255bdd6f505949af794a
ritikdolly <devwithritik200@gmail.com>
2026-03-09 12:05:31 +0530

    problem 3129. Find All Possible Stable Binary Arrays I

---
+++ march2026/FindAllPossibleStableBinaryArraysI.java
@@ -0,0 +1,43 @@
+// Date: 09-03-2026
+// 1981. Find All Possible Stable Binary Arrays I
+// https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/description/?envType=daily-question&envId=2026-03-09
+public class FindAllPossibleStableBinaryArraysI {
+    class Solution {
+        public int numberOfStableArrays(int zero, int one, int limit) {
+
+            int MOD = 1_000_000_007;
+
+            long[][][] dp = new long[zero + 1][one + 1][2];
+
+            for (int i = 1; i <= Math.min(zero, limit); i++)
+                dp[i][0][0] = 1;
+
+            for (int j = 1; j <= Math.min(one, limit); j++)
+                dp[0][j][1] = 1;
+
+            for (int i = 1; i <= zero; i++) {
+                for (int j = 1; j <= one; j++) {
+
+                    dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+
+                    if (i - limit - 1 >= 0)
+                        dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+
+                    dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+
+                    if (j - limit - 1 >= 0)
+                        dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                }
+            }
+
+            return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+        }
+    }
+    public static void main(String[] args) {
+        FindAllPossibleStableBinaryArraysI solution = new FindAllPossibleStableBinaryArraysI();
+        int zero = 2, one = 1, limit = 1;
+        int result = solution.new Solution().numberOfStableArrays(zero, one, limit);
+        System.out.println(result); // Output: 3
+    }
+
+}
