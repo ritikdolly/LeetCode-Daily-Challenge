02053a3ff736f45807ba639f1089a31034850741
ritikdolly <devwithritik200@gmail.com>
2026-04-19 00:30:34 +0530

    3783. Mirror Distance of an Integer

---
+++ april2026/MirrorDistanceofanInteger.java
@@ -0,0 +1,17 @@
+// Date : 19-04-2026
+// 3783. Mirror Distance of an Integer
+// https://leetcode.com/problems/mirror-distance-of-an-integer/?envType=daily-question&envId=2026-04-18
+public class MirrorDistanceofanInteger {
+    class Solution {
+        static public int mirrorDistance(int n) {
+            int rev = 0;
+            for (int x = n; x > 0; x /= 10) {
+                rev = 10 * rev + x % 10;
+            }
+            return Math.abs(rev - n);
+        }
+    }
+    public static void main(String[] args) {
+        // write main method code here
+    }
+}
