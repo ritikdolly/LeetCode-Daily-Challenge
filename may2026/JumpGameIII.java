309c66b1f70f1ddc59219dcf6605c36e3f0e525d
ritikdolly <devwithritik200@gmail.com>
2026-05-18 00:04:05 +0530

    1306. Jump Game III

--- may2026/JumpGameIII.java
+++ may2026/JumpGameIII.java
@@ -2,9 +2,41 @@
 // 1306. Jump Game III
 // https://leetcode.com/problems/jump-game-iii/description/?envType=daily-question&envId=2026-05-17
 
-public class JumpGameIII {
-    
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+public class JumpGameIII {
+
+    // method 1: BFS
+    // class Solution {
+    //     public boolean canReach(int[] arr, int start) {
+    //         int n = arr.length;
+
+    //         boolean[] visited = new boolean[n];
+    //         Queue<Integer> q = new ArrayDeque<>();
+
+    //         q.offer(start);
+
+    //         while (!q.isEmpty()) {
+    //             int i = q.poll();
+
+    //             if (i < 0 || i >= n || visited[i])
+    //                 continue;
+
+    //             if (arr[i] == 0)
+    //                 return true;
+
+    //             visited[i] = true;
+
+    //             q.offer(i + arr[i]);
+    //             q.offer(i - arr[i]);
+    //         }
+
+    //         return false;
+    //     }
+    // }
+
+    // method 2: DFS
     class Solution {
         public boolean canReach(int[] arr, int start) {
             if (start < 0 || start >= arr.length || arr[start] < 0)
